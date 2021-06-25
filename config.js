312e3c857575dfbda73c014e7fd12ee40ac36a92
Agustin Valdes <42925417+dasgrossfuchs@users.noreply.github.com>
2021-06-24 22:30:25 -0700

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,10 @@
+process.env.USER = 'proyectmanager';
+process.env.PWD = '4TrcYYTtXjFKuyOd';
+const uri = `mongodb+srv://${process.env.USER}:${process.env.PWD}@appis.65uar.mongodb.net/bdtest?retryWrites=true&w=majority`;
 module.exports={
     port: process.env.PORT || 3000,
     // db: process.env.MONGODB || 'mongodb://localhost:27017/BdTest',
-    db: process.env.MONGODB || 'mongodb://localhost:27017/BdTest',
+    db: process.env.MONGODB || uri,
     urlParser : { 
         useNewUrlParser: true,
         useUnifiedTopology: true, 
