e41352f55a7d503aa75107fbc505524e7f3fe6e7
Agustin Valdes <42925417+dasgrossfuchs@users.noreply.github.com>
2021-06-26 20:30:50 -0700

    crud final

--- routes/router.js
+++ routes/router.js
@@ -1,12 +1,11 @@
 /* jshint esversion: 6*/
 
-//import modules
+//////      Módulos y Controllers     //////
 const express = require('express');
 const path = require('path');
-const Product = require('../models/product');//esquema
-const product = require('../models/product');
-const authMW = require('../MiddleWare/authmw');
-const reIfAuth = require('../MiddleWare/reIfAuth');
+const expressSession = require('express-session');
+
+
 const lOc = require('../controllers/logOut');
 const putPrc = require('../controllers/putProduct');
 const delPrc = require('../controllers/delProduct');
@@ -15,12 +14,14 @@ const regUserV = require('../controllers/newuser');
 const lIV = require('../controllers/login');
 const lIC = require('../controllers/loginuser');
 
-//Create a routes object
+
+const authMW = require('../MiddleWare/authmw');
+const reIfAuth = require('../MiddleWare/reIfAuth');
+
+
 const router = express.Router();
-//export our router
 module.exports = router;
 router.use('/static', express.static('public'));
-const expressSession = require('express-session');
 
 //Activacion de las sesiones (cookies)
 router.use(expressSession({
@@ -28,44 +29,35 @@ router.use(expressSession({
     resave:true,
     saveUninitialized:true
 }));
+
 //Variables Globales
 router.use((req,res,next)=>{
     res.locals.loggedIn = req.session.userId || null;
     next();
 });
 
-//consulta por filtro
-router.get('/api/product/:productId',authMW,(req,res)=>{
-    let datoBusqueda = req.params.productId;
-    // Product.findOne({price:datoBusqueda},(err,products)=>{
-        // Product.find({price:datoBusqueda},(err,products)=>{
-        Product.findById(datoBusqueda,(err,resultado)=>{
-        if(err) return res.status(500).send({
-            message:`Error al realizar la petición : ${err}`
-        });
-        if(!resultado) return res.status(404).send({
-            message:'El producto no existe'
-        });
-        // res.status(200).send({product: products});
-        res.render('editar',{products:resultado});
-    }).lean();
-    //Agustin Valdés Fuchs
-});
-//ejemplo de consulta de todos los datos
-router.get('/api/product',authMW,(req,res)=>{
-    Product.find({},(err,resultado)=>{
-        if(err) return res.status(500).send({
-            message:`Error al realizar la petición : ${err}`
-        });
-        if(!resultado) return res.status(404).send({
-            message: 'No existen resultados'
-        })
-        // res.status(200).send({product:[resultado]});
-        res.render('showproducts',{resultado});
-    }).lean();
+
+////// RENDERS Y RUTAS  //////
+
+router.get('/',(req,res)=>{
+    res.render('home');
 });
 
-//ejemplo insertar
+router.get('/user/register',reIfAuth,regUserV);
+router.get('/user/login',reIfAuth,lIV);
+router.get('/auth/logout',lOc);
+
+router.get('/insertproduct',authMW,(req,res)=>{
+    res.render('product');
+});
+
+router.post('/auth/register',reIfAuth,regUserC);
+router.post('/auth/login',reIfAuth,lIC);
+
+//////   CRUD   //////
+const Product = require('../models/product');
+
+//Creacion de registro
 router.post('/api/product',authMW,(req,res)=>{
     let product = new Product();
     product.name = req.body.name;
@@ -83,41 +75,46 @@ router.post('/api/product',authMW,(req,res)=>{
     });
 });
 
-//Ejemplo modificacion de producto
-router.put('/api/product/:productId',authMW,putPrc);
+//Consulta general
+router.get('/api/product',authMW,(req,res)=>{
+    Product.find({},(err,resultado)=>{
+        if(err) return res.status(500).send({
+            message:`Error al realizar la petición : ${err}`
+        });
+        if(!resultado) return res.status(404).send({
+            message: 'No existen resultados'
+        })
+        // res.status(200).send({product:[resultado]});
+        res.render('showproducts',{resultado});
+    }).lean();
+});
 
-//Borrar un registro
+//Consulta filtrada
+router.get('/api/product/:productId',authMW,(req,res)=>{
+    let datoBusqueda = req.params.productId;
+    // Product.findOne({price:datoBusqueda},(err,products)=>{
+        // Product.find({price:datoBusqueda},(err,products)=>{
+        Product.findById(datoBusqueda,(err,resultado)=>{
+        if(err) return res.status(500).send({
+            message:`Error al realizar la petición : ${err}`
+        });
+        if(!resultado) return res.status(404).send({
+            message:'El producto no existe'
+        });
+        // res.status(200).send({product: products});
+        res.render('editar',{products:resultado});
+    }).lean();
+    //Agustin Valdés Fuchs
+});
+
+//Update de registro
+router.put('/api/product/:productId',authMW,putPrc);
+//Delete de registro
 router.delete('/api/product/:productId',authMW,delPrc);
 
 
 
-////// RENDERS Y RUTAS  /////
-
-//pagina home
-router.get('/',(req,res)=>{
-    // console.log(req.session);
-    res.render('home');
-    // res.status(200).send('hola mundo')
-});
-router.get('/insertproduct',authMW,(req,res)=>{
-    res.render('product');
-    // res.status(200).send('hola mundo')
-});
-router.get('/auth/logout',lOc);
-
-//pagina login
-// router.get('/login',(req,res)=>{
-//     res.status(200).send('soy login');
-// });
-
-router.get('/user/register',reIfAuth,regUserV);
-router.post('/auth/register',reIfAuth,regUserC);
-router.get('/user/login',reIfAuth,lIV);
-router.post('/auth/login',reIfAuth,lIC);
-
-
-
-//este va hasta el final para evitar problemas de enrutamiento
+//No se encontro
 router.use((req,res)=>{
     res.status(404).render('notfound');
     // console.log(res.statusCode + res);
